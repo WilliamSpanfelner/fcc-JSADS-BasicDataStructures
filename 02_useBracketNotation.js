d2c045d6e4c9556c3c7e8a90263a404260a52d7f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 14:36:00 +0300

    Use bracket notation to assign array item to var

--- 02_useBracketNotation.js
+++ 02_useBracketNotation.js
@@ -1,2 +1,14 @@
 // Access an Array's Contents Using Bracket 
 // Notation
+
+/* Each array item has a corresponding index 
+starting with zero for the first item. */
+
+let ourArray = ['a', 'b', 'c']
+
+/* Use bracket notation to assign an item of an 
+array to a variable or to perform some calculation
+on it */
+
+let ourVariable = ourArray[0];
+console.log(ourVariable);  // a
