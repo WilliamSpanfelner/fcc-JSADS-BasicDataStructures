308af5d79ccff8f59c2e445fcb167253acc35374
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 15:04:55 +0300

    Challenge - solution

--- 03_pushUnshiftArrayItems.js
+++ 03_pushUnshiftArrayItems.js
@@ -21,9 +21,11 @@ numbers 1 - 9 in order. */
 
 function mixedNumbers(arr) {
     // Only change code below this line
-
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
     // Only change code above this line
     return arr;
 }
 
-console.log(mixedNumbers(['IV', 5, 'six']));
+console.log(mixedNumbers(['IV', 5, 'six']));  
+// ['I', 2, 'three', 'IV', 5, 'six', 7, 'VIII', 9]
