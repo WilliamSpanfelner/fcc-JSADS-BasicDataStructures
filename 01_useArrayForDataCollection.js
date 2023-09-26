841c9a7989345c6f352c6cd2dd639918bda07dc3
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 14:20:25 +0300

    Add a complex array

--- 01_useArrayForDataCollection.js
+++ 01_useArrayForDataCollection.js
@@ -5,3 +5,28 @@ It can also contain a mixture of data types. */
 
 let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 console.log(simpleArray.length);  // 7
+
+let complexArray = [
+    [
+        {
+            one: 1,
+            two: 2
+        },
+        {
+            three: 3,
+            four: 4
+        }
+    ],
+    [
+        {
+            a: "a",
+            b: "b"
+        },
+        {
+            c: "c",
+            d: "d"
+        }
+    ]
+];
+
+console.log(complexArray.length);  // 2
