302f9632d94be98de9f31820d18087aadb99e0fc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 18:51:35 +0300

    Copy array items to new array

--- 07_copyWithSlice.js
+++ 07_copyWithSlice.js
@@ -1,1 +1,11 @@
 // Copy Array Items Using slice()
+/* Since the slice() method returns an array of
+the items it extracted, these values can be assigned
+to a new array leaving the original source untouched */
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3);
+
+console.log(weatherConditions, todaysWeather);
+// ['rain', 'snow', 'sleet', 'hail', 'clear'] ['snow', 'sleet']
