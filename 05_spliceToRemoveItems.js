fd51739695c1bc45be13ca7926386ab58e0080fd
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 17:20:21 +0300

    Use splice to returna new array

--- 05_spliceToRemoveItems.js
+++ 05_spliceToRemoveItems.js
@@ -9,3 +9,8 @@ items to remove are the first two.*/
 let array = ['today', 'was', 'not', 'so', 'great'];
 array.splice(2, 2);
 console.log(array);  // ['today', 'was', 'great']
+
+// The splice() method also returns another array:
+array = ['I', 'am', 'feeling', 'really', 'happy'];
+let newArray = array.splice(3, 2);
+console.log(array, newArray);  // ['I', 'am', 'feeling'] ['really', 'happy']
