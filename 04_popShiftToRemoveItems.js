ddba99fcb7bda257dca177fd9c53a86c6d68f9a4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 15:13:53 +0300

    Remove items with pop and shift methods

--- 04_popShiftToRemoveItems.js
+++ 04_popShiftToRemoveItems.js
@@ -4,3 +4,10 @@ shift() */
 /* The pop() and shift() methods are near opposites
 of push() and unshift() although neither takes an 
 argument */
+
+let greetings = ['Whats up?', 'hello', 'see ya!'];
+greetings.pop();
+console.log(greetings);  // ['Whats up?', 'hello']
+
+greetings.shift();
+console.log(greetings);  // ['hello']
