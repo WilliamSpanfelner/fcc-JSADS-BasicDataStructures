e87883578854794e7f216076dd02c3b02228f6bc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 18:03:49 +0300

    Insert array items with splice()

--- 06_spliceToAddItems.js
+++ 06_spliceToAddItems.js
@@ -1,1 +1,10 @@
 // Add Items Using splice()
+/* The third paramater of the splice() method
+is for replacing an item or items */
+
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 1;
+
+numbers.splice(startIndex, amountToDelete, 13, 14);
+console.log(numbers);
