13ae5ea482150b3606e5e19df3f34379fe40e5d5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 14:53:46 +0300

    Push and unshift array items

--- 03_pushUnshiftArrayItems.js
+++ 03_pushUnshiftArrayItems.js
@@ -1,1 +1,13 @@
-/* Add Items to an Array with push() and unshift()
+/* Add Items to an Array with push() and unshift() */
+
+/* The push and unshift methods take one or more items
+and add them to an array, either at the end or the 
+beginning respectively. */
+
+let twentyThree = "XXIII";
+let romanNumerals = ['XXI', 'XXII'];
+romanNumerals.unshift('XIX', 'XX');
+console.log(romanNumerals);  // ['XIX', 'XX', 'XXI', 'XXII']
+
+romanNumerals.push(twentyThree);
+console.log(romanNumerals);  // ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
