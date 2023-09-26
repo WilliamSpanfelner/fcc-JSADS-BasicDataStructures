5420a753dde27fdfb3fb4938c7ee79d9a8c38fb8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 14:15:14 +0300

    Add a simple array data structure

--- 01_useArrayForDataCollection.js
+++ 01_useArrayForDataCollection.js
@@ -1,1 +1,7 @@
 // Use an Array to Store a Collection of Data
+
+/* An array data structure can be one or multi-dimensional.
+It can also contain a mixture of data types. */
+
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);  // 7
