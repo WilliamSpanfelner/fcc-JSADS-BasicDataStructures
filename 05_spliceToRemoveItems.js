16972a31e1ef2cd133bdbfb4d6f7f4d1725126a5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 17:16:26 +0300

    Remove elements from within an array

--- 05_spliceToRemoveItems.js
+++ 05_spliceToRemoveItems.js
@@ -1,1 +1,11 @@
 // Remove Items Using splice()
+/* The splice method will remove any number
+of consecutive elements from anywhere within 
+the array. 
+
+The splice() takes up to three parameters: an index 
+for where to start removing items, and the number of 
+items to remove are the first two.*/
+let array = ['today', 'was', 'not', 'so', 'great'];
+array.splice(2, 2);
+console.log(array);  // ['today', 'was', 'great']
