b6d1fc600344ce373dbafb7ca1d20c620d895a38
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 18:11:37 +0300

    Challenge - solution

--- 06_spliceToAddItems.js
+++ 06_spliceToAddItems.js
@@ -17,7 +17,7 @@ in their respective places. */
 
 function htmlColorNames(arr) {
     // Only change code below this line
-
+    arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
     // Only change code above this line
     return arr;
 }
@@ -26,3 +26,4 @@ console.log(htmlColorNames(
     ['DarkGoldenRod', 'WhiteSmoke',
     'LavenderBlush', 'PaleTurquoise',
     'FireBrick']));
+    // ['DarkSalmon', 'BlanchedAlmond', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']
