9f886e17fa7c589463faddfc2f04fe7436b5d572
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 18:56:40 +0300

    Challenge - start

--- 07_copyWithSlice.js
+++ 07_copyWithSlice.js
@@ -9,3 +9,18 @@ let todaysWeather = weatherConditions.slice(1, 3);
 
 console.log(weatherConditions, todaysWeather);
 // ['rain', 'snow', 'sleet', 'hail', 'clear'] ['snow', 'sleet']
+
+/* We have defined a function, forecast, that takes an array
+as an argument. Modidy the function using slice() to 
+extract information from the argument array and return a 
+new array that contains the string elements warm and 
+sunny. */
+
+function forecast(arr) {
+    // Only change code below this line
+
+    return arr;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
