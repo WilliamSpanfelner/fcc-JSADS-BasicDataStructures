e88f79594b163c1a3dab92f7117beafabb986fc2
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-26 17:56:12 +0300

    Challenge - solution

--- 05_spliceToRemoveItems.js
+++ 05_spliceToRemoveItems.js
@@ -21,6 +21,12 @@ sum to the value of 10. */
 
 const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 // Only change code below this line
+arr.splice(0, 2);
+arr.splice(1, 2);
+arr.splice(2, 2);
 
+// for (let i = 0; i < 3; i++) {
+//     arr.splice(i, 2);
+// }
 // Only change code above this line
-console.log(sum, arr);
+console.log(arr);  // [5, 5]
