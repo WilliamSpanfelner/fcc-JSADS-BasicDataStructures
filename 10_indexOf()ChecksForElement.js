5cfa5c94d68d983cd178e5d21ceee3f41e40f39a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 12:04:04 +0300

    Challenge - start

--- 10_indexOf()ChecksForElement.js
+++ 10_indexOf()ChecksForElement.js
@@ -9,3 +9,18 @@ let fruit = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
 console.log(fruit.indexOf('dates'));  // -1
 console.log(fruit.indexOf('oranges'));  // 2
 console.log(fruit.indexOf('pears'));  // 1
+
+/* indexOf() can be incredibly useful for quickly checking
+for the presence of an element on an array.  We have defined
+a function, quickCheck, that takes and array and an element
+as arguments. Modify the function using indexOf() so that it
+returns true if the passed element exists on the array, 
+and false if it does not. */
+
+function quickCheck(arr, elem) {
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
