6487be3e2a6408bcf5f89a66487c1dbc841a4ef2
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 09:44:26 +0300

    Challenge - start

--- 09_combineArraysWithSpread.js
+++ 09_combineArraysWithSpread.js
@@ -5,3 +5,16 @@ let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
 
 let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
 console.log(thatArray);  // ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+
+/* We have defined a function spreadOut that returns the
+variable sentence.  Modify the function using the spread
+operator so that it returns the array ['learning', 
+'to', 'code', 'is', 'fun']. */
+
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence; // Change this line
+    return sentence
+}
+
+console.log(spreadOut());
