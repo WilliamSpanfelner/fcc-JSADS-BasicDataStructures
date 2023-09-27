75785b038bdf34b0a1fc2b0f83e67a752e6dd937
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 17:47:42 +0300

    Add a more complex object

--- 14_modifyNestedObject.js
+++ 14_modifyNestedObject.js
@@ -1,1 +1,15 @@
 // Modify an Object Nested Within an Object
+// Consider this object:
+let nestedObject = {
+    id: 2880269164,
+    date: 'December 31, 2016',
+    data: {
+        totalUsers: 99,
+        online: 80,
+        onlineStatus: {
+            active: 67,
+            away: 13,
+            busy: 8
+        }
+    }
+};
