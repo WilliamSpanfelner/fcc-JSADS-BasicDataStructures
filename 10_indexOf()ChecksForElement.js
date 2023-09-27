cbb64e036dbf476f5af3ec9d39da9defd3642813
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 11:59:34 +0300

    Find element indices with indexOf()

--- 10_indexOf()ChecksForElement.js
+++ 10_indexOf()ChecksForElement.js
@@ -1,3 +1,11 @@
 /* Check for the Presence of an Element With 
 indexOf() */
 
+/* indexOf() takes an element as an argument
+and returns the first element index or -1 if the 
+element doesn't exist. */
+
+let fruit = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+console.log(fruit.indexOf('dates'));  // -1
+console.log(fruit.indexOf('oranges'));  // 2
+console.log(fruit.indexOf('pears'));  // 1
