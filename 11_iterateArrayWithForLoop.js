fc40d18e67e51801f529c8804d45c75071c45aea
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 12:18:41 +0300

    Iterate and find array elements > 10

--- 11_iterateArrayWithForLoop.js
+++ 11_iterateArrayWithForLoop.js
@@ -1,1 +1,17 @@
 /* Iterate through an Array's Items Using For Loops
+
+/* A simple for loop offers greatest control over other
+methods of iterating over arrays. */
+
+function greaterThanTen(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 10) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+const result = greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
+console.log(result); // [12, 14, 80]
