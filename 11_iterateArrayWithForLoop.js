300f2dfd49cb364a3b16a25db76ecc9887979611
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 12:25:08 +0300

    Challeng - start

--- 11_iterateArrayWithForLoop.js
+++ 11_iterateArrayWithForLoop.js
@@ -15,3 +15,21 @@ function greaterThanTen(arr) {
 
 const result = greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
 console.log(result); // [12, 14, 80]
+
+/* We have defined a function, filteredArray, which takes
+arr, a nested array, and elem as arguments, and returns a 
+new array.  elem represents an elemnent that may or may
+not be present on one or more of the arrays nested with 
+arr.  Modify the function, using a for loop, to return a
+filtered version of the passed array such that any array
+nested within arr containing elem has been removed. */
+
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+
+    // Only change code above this line
+    return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
