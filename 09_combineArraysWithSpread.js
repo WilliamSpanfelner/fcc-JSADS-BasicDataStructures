eddf71cf8539a05c4c8009495b049c37a99564a4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 09:40:27 +0300

    Insert one array in another with spread

--- 09_combineArraysWithSpread.js
+++ 09_combineArraysWithSpread.js
@@ -1,1 +1,7 @@
 // Combine Arrays with the Spread Operator
+// Insert one array in another with the spread operator
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+console.log(thatArray);  // ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
