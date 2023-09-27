77736cafda21962d106550fd6699abe2871eca22
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 09:29:36 +0300

    Challenge - start

--- 08_copyArrayWithSpread.js
+++ 08_copyArrayWithSpread.js
@@ -5,3 +5,25 @@ let thisArray = [true, true, undefined, false, null];
 
 let thatArray = [...thisArray];
 console.log(thatArray);  // [true, true, undefined, false, null]
+
+/* We have defined a function, copyMachine which takes 
+arr (an array) and num (a number) as arguments.  The 
+function is supposed to return a new array made up of num
+copies of arr.  We have done most ofthe work for you, but
+it doesn't work quite right yet.  Modify the function using
+spread syntax so that it works correctly (hint: another
+method we have already covered might come in handy
+here!). */
+
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
