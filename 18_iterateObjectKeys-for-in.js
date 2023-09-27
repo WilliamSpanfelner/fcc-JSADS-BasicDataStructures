56413ced5ba301fadf5cd7ac4becb49ab489120b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 20:07:44 +0300

    Iterate object with for in statement

--- 18_iterateObjectKeys-for-in.js
+++ 18_iterateObjectKeys-for-in.js
@@ -1,2 +1,11 @@
 /* Iterate through the keys of an object with a 
 for ... in statement */
+
+const refrigerator = {
+    'milk': 1,
+    'eggs': 12
+};
+
+for (const food in refrigerator) {
+    console.log(food, refrigerator[food]);
+}
