680ee5b2d8a08f6d615495bcda40126ba894208c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 19:30:25 +0300

    Delete a key-value pair

--- 16_deleteObjectKeys.js
+++ 16_deleteObjectKeys.js
@@ -1,1 +1,21 @@
 // Use the delete Keyword to Remove Object Properties
+let fruit = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+delete fruit.apples;
+console.log(fruit);
+/*
+{
+    oranges: 32, 
+    plums: 28, 
+    bananas: 13, 
+    grapes: 35, 
+    strawberries: 27
+}
+*/
