4a1f8419aa0e89eb13e3b2d55e0470ae19f0d120
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 16:54:16 +0300

    Reset an array element value

--- 12_createComplexMultiDimensionalArrays.js
+++ 12_createComplexMultiDimensionalArrays.js
@@ -19,3 +19,8 @@ let nestedArray = [
 ];
 
 console.log(nestedArray[2][1][0][0][0]);  // deepest-est?
+
+// Now let reset the value 5-levels in:
+nestedArray[2][1][0][0][0] = 'deeper still';
+
+console.log(nestedArray[2][1][0][0][0]);  // deeper still
