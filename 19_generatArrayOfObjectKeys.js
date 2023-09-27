e0a30d4898127b7a78358d0fd47ecd2c2531d33f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 20:58:32 +0300

    Challenge - start

--- 19_generatArrayOfObjectKeys.js
+++ 19_generatArrayOfObjectKeys.js
@@ -1,7 +1,7 @@
 /* Generate an Array of All Object Keys with 
 Object.keys() */
 
-const users = {
+const oldUsers = {
     Alan: {
         online: false
     },
@@ -14,4 +14,35 @@ const users = {
 };
 
 // Object.keys() method generates an array of object keys.
-console.log(Object.keys(users));  // ['Alan', 'Jeff', 'Sarah']
+console.log(Object.keys(oldUsers));  // ['Alan', 'Jeff', 'Sarah']
+
+/* Finish writing the getArrayOfUsers function so that it
+returns an array containing all the properties in the object it
+receives as an argument. */
+
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function getArrayOfUsers(obj) {
+    // Only change code below this line
+    
+    // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
