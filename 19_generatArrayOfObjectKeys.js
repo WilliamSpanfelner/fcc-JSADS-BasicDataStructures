4fa2578822f539bde2d593175fd883cc0752c57f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 20:43:35 +0300

    Illustrate object.keys() method

--- 19_generatArrayOfObjectKeys.js
+++ 19_generatArrayOfObjectKeys.js
@@ -1,3 +1,17 @@
 /* Generate an Array of All Object Keys with 
 Object.keys() */
 
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+};
+
+// Object.keys() method generates an array of object keys.
+console.log(Object.keys(users));  // ['Alan', 'Jeff', 'Sarah']
