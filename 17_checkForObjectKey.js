2f67973099198aec894c5db485f764ef4198e435
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 19:47:21 +0300

    Check for keys in object

--- 17_checkForObjectKey.js
+++ 17_checkForObjectKey.js
@@ -1,1 +1,16 @@
 // Check if an Object has a Property
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+
+/* There are two ways to check for keys or properties
+in an object: the hasOwnProperty() method and the in 
+keyword. */
+
+console.log(foods.hasOwnProperty('oranges'));  // true
+console.log('oranges' in foods);  // true
