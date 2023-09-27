a2715442ee45654e16f04daf9e8ca27f2a5091b5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 17:27:19 +0300

    Add a new key-value pair to the object

--- 13_addKVPairsToJSObjects.js
+++ 13_addKVPairsToJSObjects.js
@@ -1,8 +1,20 @@
 // Add Key-Value Pairs to JavaScript Objects 
 // Consider the following object:
-
 const tekkenCharacter = {
     player: 'Hwoarang',
     fightingStyle: 'Tae Kwon Doe',
     human: true
 };
+
+// now let's add a new key-value pair:
+
+tekkenCharacter.origin = 'South Korea';
+console.log(tekkenCharacter);
+/* 
+{
+    player: 'Hwoarang',
+    fightingStyle: 'Tae Kwon Doe', 
+    human: true, 
+    origin: 'South Korea'
+} 
+*/
