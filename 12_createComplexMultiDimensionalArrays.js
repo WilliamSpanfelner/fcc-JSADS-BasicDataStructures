46af47305986c87a416e21d0e07578ac87e654a5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 16:51:43 +0300

    Add complex array example

--- 12_createComplexMultiDimensionalArrays.js
+++ 12_createComplexMultiDimensionalArrays.js
@@ -1,1 +1,21 @@
 // Create complex multi-dimensional arrays
+// Consider the following complex array:
+
+let nestedArray = [
+    ['deep'],
+    [
+        ['deeper'], ['deeper']
+    ],
+    [
+        [
+            ['deepest'], ['deepest']
+        ],
+        [
+            [
+                ['deepest-est?']
+            ]
+        ]
+    ]
+];
+
+console.log(nestedArray[2][1][0][0][0]);  // deepest-est?
