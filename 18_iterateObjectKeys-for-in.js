38a199bca0c991c2c441f41f4071c180cf98f5f1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 20:14:19 +0300

    Challenge - start

--- 18_iterateObjectKeys-for-in.js
+++ 18_iterateObjectKeys-for-in.js
@@ -9,3 +9,31 @@ const refrigerator = {
 for (const food in refrigerator) {
     console.log(food, refrigerator[food]);
 }
+
+/* We've defined a function countOnline which accepts one
+argument, allUsers.  Use a for...in statement inside this
+function to loop through the allUsers object and return
+the number of users whose online property is set to
+true.  An example of an object which could be passed to
+countOnline is shown below.  Each user will have an online
+property set to either true or false. */
+
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+};
+
+function countOnline(allUsers) {
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+console.log(countOnline(users));
