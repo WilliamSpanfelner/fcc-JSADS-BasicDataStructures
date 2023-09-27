4e68a5ef199374b3f724c2e0ae16ea475b44a869
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 17:56:41 +0300

    Challenge - solution

--- 14_modifyNestedObject.js
+++ 14_modifyNestedObject.js
@@ -32,7 +32,12 @@ let userActivity = {
 };
 
 // Only change code below this line
-
+userActivity.data.online = 45;
 // Only change code below this line
 
 console.log(userActivity);
+/*
+{ id: 23894201352,
+  date: 'January 1, 2017',
+  data: { totalUsers: 51, online: 45 } }
+*/
