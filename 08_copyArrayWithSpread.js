b473bcb470dc3f247e5f1e72f79bb49c1a716b88
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-27 09:23:24 +0300

    Copy array with spread operator

--- 08_copyArrayWithSpread.js
+++ 08_copyArrayWithSpread.js
@@ -1,1 +1,7 @@
 // Copy an Array with the Spread Operator
+// The spread operator is written like this ...
+
+let thisArray = [true, true, undefined, false, null];
+
+let thatArray = [...thisArray];
+console.log(thatArray);  // [true, true, undefined, false, null]
